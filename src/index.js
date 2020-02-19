f6d209123f61d5dfa9b639faf72e795d8dc10632
starff <starff.st@gmail.com>
2020-02-19 23:08:24 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-
+function rightSort(a, b) {
+    if (a > b) return 1; 
+    if (a == b) return 0;
+    if (a < b) return -1;
+};
 exports.min = function min (array) {
-  return 0;
+    if(array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        array.sort(rightSort);
+        return array[0];
+    };
+    
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || array.length === 0) {
+        return 0;
+    } else {
+    array.sort(rightSort).reverse();
+    return array[0];
+    };
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || array.length === 0) {
+        return 0;
+    } else {
+    let all = array.reduce((sum, current) => sum + current, 0);
+    return all/array.length;
+    };
 }
